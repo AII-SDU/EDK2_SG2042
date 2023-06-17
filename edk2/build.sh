f9e73cdddafa43cd5199c049bbf02af2d6d08a40
assassin <assassin@cislc.local>
2023-06-17 16:19:32 +0800

    delete make script

--- edk2/build.sh
+++
@@ -1,2 +0,0 @@
-make -C BaseTools
-build -a RISCV64 -n 12 -t GCC5 -p OvmfPkg/RiscVVirt/RiscVVirtQemu.dsc
--- edk2/myexport.sh
+++
@@ -1,5 +0,0 @@
-export WORKSPACE=/home/cyq/Downloads/edk2-edk2-stable202305
-export GCC5_RISCV64_PREFIX=/mnt/share/riscv_gnu/bin/riscv64-unknown-elf-
-export PYTHON_COMMAND=/usr/bin/python3
-
-
